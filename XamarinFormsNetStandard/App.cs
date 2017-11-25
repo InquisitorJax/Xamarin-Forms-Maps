1c187b86fd1708119757252aa53f451f831888ed
Malcolm Jack <inquisitorjax@gmail.com>
2017-11-25 07:51:02 +0200

    initial commit

---
+++ XamarinFormsNetStandard/App.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+    }
+}
